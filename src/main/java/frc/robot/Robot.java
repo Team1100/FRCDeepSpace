2463c361af5a2824e0d7afed33cc668876b69fe7
Sriram Tolety <tsriram18@gmail.com>
2019-01-28 18:48:45 -0500

    Added init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.BallIntake;
+import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Elevator;
 
@@ -42,6 +44,8 @@ public class Robot extends TimedRobot {
     m_oi = new OI();
     Drive.getInstance();
     Elevator.getInstance();
+    Claw.getInstance();
+    BallIntake.getInstance();
     
     // m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
