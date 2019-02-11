8862b66dc239fa14283d6816e091e3a6229d1517
Sriram Tolety <tsriram18@gmail.com>
2019-02-11 17:21:29 -0500

    Added rough testing for PW, needs work

--- src/main/java/frc/robot/commands/auto/TestAutoPathCommand.java
+++ src/main/java/frc/robot/commands/auto/TestAutoPathCommand.java
@@ -7,6 +7,8 @@
 
 package frc.robot.commands.auto;
 
+import java.io.File;
+
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Notifier;
@@ -19,9 +21,45 @@ import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.followers.EncoderFollower;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Drive;
 
 public class TestAutoPathCommand extends Command {
+
+  EncoderFollower leftFollower;
+  EncoderFollower rightFollower;
+
+  Drive chassis = Drive.getInstance();
+
+  File leftCSV;
+  File rightCSV;
+
+  Trajectory rightTrajectory;
+  Trajectory leftTrajectory;
+
+  Notifier notifier;
+
+  double dt;
+
+  double p = 0.8;
+  double i = 0.;
+  double d = 0;
+  double v = 1 / 12;
+  double a = 0;
+  
+  
   public TestAutoPathCommand() {
+    requires(Drive.getInstance());
+
+    leftCSV = new File("/home/****/deploy/" + pathName + ".left");
+    rightCSV = new File("/home/*****/deploy/" + pathName + ".right");
+
+    leftTrajectory = Pathfinder.readFromCSV(leftCSV);
+    rightTrajectory = Pathfinder.readFromCSV(rightCSV);
+
+    notifier = new Notifier(new RunProfile());
+    dt = leftTrajectory.get(0).dt;
+
+    System.out.println("CSV has been locked and loaded");
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -29,6 +67,20 @@ public class TestAutoPathCommand extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    leftFollower = new EncoderFollower(leftTrajectory);
+    rightFollower = new EncoderFollower(rightTrajectory);
+
+    leftFollower.reset();
+    rightFollower.reset();
+
+    leftFollower.configureEncoder((int)Drive.getEncoderLeft(), 128, 6 / 12);
+    rightFollower.configureEncoder((int)Drive.getEncoderRight(), 128, 6 / 12);
+
+    leftFollower.configurePIDVA( p,  i, d, v, a);
+    rightFollower.configurePIDVA(p , i, d , v , a);
+    notifier.startPeriodic(dt);
+
+    System.out.println("Initialized");
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -39,17 +91,44 @@ public class TestAutoPathCommand extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    System.out.println("Finished");
+    return leftFollower.isFinished() && rightFollower.isFinished();
+    //return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    notifier.stop();
+    chassis.tankDrive(0,0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+  }
+  class RunProfile implements java.lang.Runnable{
+    int segmentNumber = 0;
+    
+    @Override
+    public void run(){
+    
+    
+    double leftOutput = leftFollower.calculate((int) chassis.getEncoderLeft());
+    double rightOutput = rightFollower.calculate((int) chassis.getEncoderRight());
+    
+    double gyroHeading = 0;
+    
+    double desiredHeading = Pathfinder.d2r(leftFollower.getHeading());
+    
+    double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
+    
+    double turn = 0.08 *  (-1. / 80.) * angleDifference;
+    
+    chassis.tankDrive(leftOutput - turn, rightOutput - turn);
+    
+    segmentNumber++;
+    }
   }
 }
