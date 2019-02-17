1d07b8ab008cc2cc45275b6353f897f8cab8b214
Thor Smith <thorsmith777@gmail.com>
2019-02-17 14:25:08 -0500

    fix functions in BallIntake subsystem
    This change removes a while loop in the lowerIntake
    method and adds/adjusts some additional methods.

--- src/main/java/frc/robot/subsystems/Stilts.java
+++ src/main/java/frc/robot/subsystems/Stilts.java
@@ -7,7 +7,6 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
