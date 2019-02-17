ce54e58e7f305fa5f55529368aae97b08e145722
Thor Smith <thorsmith777@gmail.com>
2019-02-17 12:50:19 -0500

    add command for balancing stilts with motors based on pitch

--- src/main/java/frc/robot/subsystems/Stilts.java
+++ src/main/java/frc/robot/subsystems/Stilts.java
@@ -8,6 +8,8 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
@@ -18,19 +20,24 @@ public class Stilts extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  private TalonSRX leftMotor;
-  private TalonSRX rightMotor;
+  private WPI_TalonSRX leftMotor;
+  private WPI_TalonSRX rightMotor;
   private static Stilts stilts;
 
   private Stilts() {
-    leftMotor = new TalonSRX(RobotMap.S_LEFT);
-    rightMotor = new TalonSRX(RobotMap.S_RIGHT);
+    leftMotor = new WPI_TalonSRX(RobotMap.S_LEFT);
+    rightMotor = new WPI_TalonSRX(RobotMap.S_RIGHT);
   }
 
   public static Stilts getInstance() {
 		if (stilts == null) stilts = new Stilts();
 		return stilts;
-	}
+  }
+  
+  public void setSpeed(double speed) {
+    leftMotor.set(speed);
+    rightMotor.set(speed);
+  }
 
   @Override
   public void initDefaultCommand() {
