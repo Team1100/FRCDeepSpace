640159703ca016606d99ab39766244b4160d486c
Sriram Tolety <tsriram18@gmail.com>
2019-02-25 19:09:38 -0500

    test

--- src/main/java/frc/robot/commands/SwitchSides.java
+++ src/main/java/frc/robot/commands/SwitchSides.java
@@ -28,8 +28,6 @@ public class SwitchSides extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    
-
   }
 
   // Called repeatedly when this Command is scheduled to run
