abb935049f10541c1ed8c3c7739e658f079b84ed
Sriram Tolety <tsriram18@gmail.com>
2019-02-25 19:45:14 -0500

    test

--- src/main/java/frc/robot/commands/rollers/RollersIn.java
+++ src/main/java/frc/robot/commands/rollers/RollersIn.java
@@ -43,13 +43,14 @@ public class RollersIn extends Command {
   @Override
   protected boolean isFinished() {
     return isTimedOut();
-  }
+  } 
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     rollers.rollersIn(0);
   }
+  
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
