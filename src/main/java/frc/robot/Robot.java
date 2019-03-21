6faf5c155f430fd130f22a950017ebcb4464c60f
Thor Smith <thorsmith777@gmail.com>
2019-03-20 20:25:03 -0400

    moves testing dashboard to its own class
    This change moves the testing dashboard to
    its own class. This will make it easier to
    use debuggable values on the testing
    dashboard.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -40,13 +40,12 @@ import frc.robot.subsystems.*;
 public class Robot extends TimedRobot {
   public static OI m_oi;
   public static PowerDistributionPanel pdp;
+  private TestingDashboard testingDashboard;
   //public static Vision vision;
 
   Command autoCommand;
   SendableChooser<Command> auto_chooser = new SendableChooser<>();
   CameraServer cs;
-
-  NetworkTableEntry isClosed, isForward, voltage, isAtLeftLimit, isAtRightLimit, isAtLevelThree, isAtLevelTwo, isAtLevelOne;
   
   /**
    * This function is run when the robot is first started up and should be
@@ -74,141 +73,11 @@ public class Robot extends TimedRobot {
     cs.startAutomaticCapture("Front Camera", 0).setResolution(540, 360);
     cs.startAutomaticCapture("Rear Camera", 1);
     
-    //setupAutoChooser();
-    createTestingDashboard();
-  }
+    // Create TestingDashboard last
+    testingDashboard = TestingDashboard.getInstance();
 
-  public void createTestingDashboard() {
-    // CLAW Commands
-    ShuffleboardTab claw_tab = Shuffleboard.getTab("Claw");
-    claw_tab.add(Claw.getInstance());
-    ShuffleboardLayout claw_g1 = claw_tab.getLayout("BasicOpen", BuiltInLayouts.kList);
-    claw_g1.add(new OpenClaw());
-    claw_g1.add(new CloseClaw());
-    ShuffleboardLayout claw_g2 = Shuffleboard.getTab("Claw").getLayout("BasicPush", BuiltInLayouts.kList);
-    claw_g2.add(new PushClawForward());
-    claw_g2.add(new PullClawBack());
-    ShuffleboardLayout claw_g3 = Shuffleboard.getTab("Claw").getLayout("Ball", BuiltInLayouts.kList);
-    claw_g3.add(new ClawAcquireBall());
-    //claw_g3.add(new CloseOnBall());
-    claw_g3.add(new AcquireBallFromIntake());
-    claw_g3.add(new ScoreCargo());
-    claw_g3.add(new LaunchBall());
-    ShuffleboardLayout claw_g4 = Shuffleboard.getTab("Claw").getLayout("Hatch", BuiltInLayouts.kList);
-    claw_g4.add(new PlaceHatch());
-    claw_g4.add(new PickupHatch());
-    claw_g4.add(new PickupHatchHPS_Vision());
-    claw_g4.add(new PickupHatchHPS());
-    claw_g4.add(new DeployHatch());
-    ShuffleboardLayout claw_g5 = Shuffleboard.getTab("Claw").getLayout("Complex", BuiltInLayouts.kList);
-    claw_g5.add(new CloseClawWhenSensed());
-
-    // DRIVE commands
-    ShuffleboardTab drive_tab = Shuffleboard.getTab("Drive");
-    drive_tab.add(Drive.getInstance());
-    ShuffleboardLayout drive_g1 = drive_tab.getLayout("Basic", BuiltInLayouts.kList);
-    drive_g1.add(new DefaultDrive());
-    drive_g1.add(new ChangeHeading(0, 0.5));
-    drive_g1.add(new MeasuredDrive(0.5, 0.5));
-    ShuffleboardLayout drive_g2 = drive_tab.getLayout("Climbing", BuiltInLayouts.kList);
-
-
-    // INTAKE commands
-    ShuffleboardTab intake_tab = Shuffleboard.getTab("Intake");
-    intake_tab.add(BallIntake.getInstance());
-    ShuffleboardLayout intake_g1 = intake_tab.getLayout("Basic", BuiltInLayouts.kList);
-    intake_g1.add(new DefaultIntake());
-    intake_g1.add(new IntakeDown());
-    intake_g1.add(new IntakeUp());
-    intake_g1.add(new StopIntake());
-    ShuffleboardLayout intake_g2 = intake_tab.getLayout("Climbing", BuiltInLayouts.kList);
-    intake_g2.add(new HoldPosition(2.0));
-    intake_g2.add(new ClimbingIntakeDown(0.5));
-    intake_g2.add(new IncrementIntakePower(0.1));
-    intake_g2.add(new ClimbWhileIntake(5, 5));
-    ShuffleboardLayout intake_g3 = intake_tab.getLayout("BallCommands", BuiltInLayouts.kList);
-    intake_g3.add(new IntakeCargo());
-    intake_g3.add(new AcquireBall());
-    intake_g3.add(new ScoreCargo_Intake());
-    intake_g3.add(new ScoreCargo_RocketL1_Intake());
-    intake_g3.add(new Intake_Down_RocketL1());
-
-    ShuffleboardLayout intake_g4 = intake_tab.getLayout("HatchCommands", BuiltInLayouts.kList);
-    intake_g4.add(new PickupHatchHPS());
-
-    // KICKER commands
-
-    // ROLLERS commands
-    ShuffleboardTab rollers_tab = Shuffleboard.getTab("Rollers");
-    rollers_tab.add(Rollers.getInstance());
-    ShuffleboardLayout rollers_g1 = rollers_tab.getLayout("Basic", BuiltInLayouts.kList);
-    rollers_g1.add(new DefaultRollers());
-    rollers_g1.add(new RollersIn());
-    rollers_g1.add(new StopRollers());
-    rollers_g1.add(new PushBallOut(5));
-    rollers_g1.add(new MoveBallToChute(5));
-
-    // VISION commands
-    ShuffleboardTab vision_tab = Shuffleboard.getTab("Vision");
-    vision_tab.add(Vision.getInstance());
-    ShuffleboardLayout vision_g1 = vision_tab.getLayout("Basic", BuiltInLayouts.kList);
-
-
-    // GANTRY commands
-    ShuffleboardTab gantry_tab = Shuffleboard.getTab("Gantry");
-    gantry_tab.add(Gantry.getInstance());
-    ShuffleboardLayout gantry_g1 = gantry_tab.getLayout("Basic", BuiltInLayouts.kList);
-    gantry_g1.add(new CenterGantry());
-    gantry_g1.add(new DefaultGantry());
-    gantry_g1.add(new GantryLeftLimit());
-    gantry_g1.add(new GantryRightLimit());
-    ShuffleboardLayout gantry_g2 = gantry_tab.getLayout("Complex", BuiltInLayouts.kList);
-    gantry_g2.add(new MoveGantryToCenter());
-    gantry_g2.add(new MoveToSetpoint(0.5));
-
-    //ELEVATOR commands
-    ShuffleboardTab elevator_tab = Shuffleboard.getTab("Elevator");
-    elevator_tab.add(Elevator.getInstance());
-    ShuffleboardLayout elevator_g1 = elevator_tab.getLayout("Basic", BuiltInLayouts.kList);
-    elevator_g1.add(new DefaultElevator());
-    elevator_g1.add(new Elevator_L1());
-    elevator_g1.add(new Elevator_Rocket_L2());
-    elevator_g1.add(new Elevator_Rocket_L3());
-    ShuffleboardLayout elevator_g2 = elevator_tab.getLayout("PID", BuiltInLayouts.kList);
-    elevator_g2.add(new PIDElevator(5));
-    elevator_g2.add(new PIDElevatorL1());
-    elevator_g2.add(new PIDElevatorL2());
-    elevator_g2.add(new PIDElevatorL3());
-
-    ShuffleboardLayout elevator_g3 = elevator_tab.getLayout("Placement", BuiltInLayouts.kList);
-    elevator_g3.add(new L1_Placement());
-    elevator_g3.add(new L2_Placement());
-    elevator_g3.add(new L3_Placement());
-
-    createDebugTab();
-  }
-
-  public void createDebugTab() {
-    ShuffleboardTab debug_tab = Shuffleboard.getTab("Debug");
-    isClosed = debug_tab.add("Claw is Closed", Claw.getInstance().isClosed()).getEntry();
-    isForward = debug_tab.add("Claw is Pushed Forward", Claw.getInstance().isForward()).getEntry();
-    voltage = debug_tab.add("Intake Pot", BallIntake.getInstance().getVoltage()).getEntry();
-    isAtLeftLimit = debug_tab.add("Left Limit Gantry", Gantry.getInstance().isAtLeftLimit()).getEntry();
-    isAtRightLimit = debug_tab.add("Right Limit Gantry", Gantry.getInstance().isAtRightLimit()).getEntry();
-    debug_tab.add("Gantry Encoder", Gantry.getInstance().getEncoder());
-    isAtLevelThree = debug_tab.add("Level 3 Switch", Elevator.getInstance().isAtLevelThree()).getEntry();
-    isAtLevelTwo = debug_tab.add("Level 2 Switch", Elevator.getInstance().isAtLevelTwo()).getEntry();
-    isAtLevelOne = debug_tab.add("Level 1 Switch", Elevator.getInstance().isAtLevelOne()).getEntry();
-  }
-
-  public void updateDebugTab() {
-    isClosed.setBoolean((Claw.getInstance().isClosed()));
-    isForward.setBoolean((Claw.getInstance().isForward()));
-    voltage.setValue(BallIntake.getInstance().getVoltage());
-    isAtLeftLimit.setBoolean(Gantry.getInstance().isAtLeftLimit());
-    isAtRightLimit.setBoolean(Elevator.getInstance().isAtLevelThree());
-    isAtLevelTwo.setBoolean(Elevator.getInstance().isAtLevelTwo());
-    isAtLevelOne.setBoolean(Elevator.getInstance().isAtLevelOne());
+    // Add components to testing dashboard
+    testingDashboard.createTestingDashboard();
   }
 
   public void setupAutoChooser() {
@@ -338,7 +207,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    updateDebugTab();
+    testingDashboard.updateDebugTab();
     Scheduler.getInstance().run();
   }
 
